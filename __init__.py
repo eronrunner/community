01da4d0f8d97ad130efca6420792de2126939059
eron.runner@gmail.com <eron.runner@gmail.com>
2020-07-21 03:25:26 +0700

    update app config, post api

--- __init__.py
+++ __init__.py
@@ -3,17 +3,28 @@ import sys
 import logging.config
 import pathlib
 
+from werkzeug.routing import BaseConverter
+
 APP_SETTINGS = os.environ.get('APP_SETTINGS', 'config.DevelopmentConfig')
-DEV_SERVER_NAME = os.environ.get('DEV_SERVER_NAME', 'localhost:5000')
+SERVER_RUN_PORT = int(os.environ.get('SERVER_RUN_PORT', '5000'))
+SERVER_RUN_HOST = os.environ.get('SERVER_RUN_HOST', 'localhost')
+PROTOCOL = 'http'
+SERVER_NAME = f'{SERVER_RUN_HOST}:{SERVER_RUN_PORT}'
 LOGGER = os.environ.get('APP_LOGGER', 'dev')
 
-HOME = str(os.path.abspath(pathlib.Path(__name__).parent))
+HOME = str(os.path.abspath(pathlib.Path(__file__).parent))
 if HOME not in sys.path:
     sys.path.append(HOME)
 
 # JSON Encoder
 
-# BaseConverter
+# URL CONVERTER
+class RegexConverter(BaseConverter):
+    def __init__(self, map, *args):
+        self.map = map
+        self.regex = args[0]
+
+REGEX_CONVERTER = RegexConverter
 
 # logging
 logging.config.fileConfig('logging.conf')
--- app.py
+++ app.py
@@ -1,10 +1,15 @@
 from flask import Flask
 
-import FlaskApp
+import __init__
+from blueprints.Post import POST
 
-APP = Flask("FlaskApp", instance_relative_config=True)
+APP = Flask("COMMUNITY", instance_relative_config=True)
 
-APP.config.from_object(FlaskApp.APP_SETTINGS)
+APP.config.from_object(__init__.APP_SETTINGS)
+
+APP.register_blueprint(POST)
 
 if __name__ == '__main__':
-    APP.run()
+        APP.run()
+        # from waitress import serve
+        # serve(APP, host='0.0.0.0', port=5000)
